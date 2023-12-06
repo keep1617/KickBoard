f3c72be169d8b62ec252a96bf951af0a5329876f
jihee0519 <152167551+jihee0519@users.noreply.github.com>
2023-12-06 13:43:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -159,29 +159,29 @@ class Judge:
         
     
     def location(self, gps_values) :
-        #GPS 값이 특공관 위치에 해당하면 True를 출력
-        #특공관 중앙의 위도, 경도 값을 중심으로 일정 범위 내에 있는지 확인
+        #GPS 값이 웅비관 킥보드 주차장 위치에 해당하면 True를 출력
+        #주차장 중앙의 위도, 경도 값을 중심으로 일정 범위 내에 있는지 확인
 
         self.GPS_val = gps_values
 
-        #특공관 중앙의 위도, 경도
-        middle_lat = 35.233280
-        middle_lon = 129.082891
+        #웅비관 킥보드 주차장 중앙의 위도, 경도
+        middle_lat = 35.237052
+        middle_lon = 129.077633
 
-        #특공관 가장 끝의 위도, 경도
-        edge_lat = 35.233061
-        edge_lon = 129.083228
+        #웅비관 킥보드 주차장 가장 끝의 위도, 경도
+        edge_lat = 35.236867
+        edge_lon = 129.077564
 
         #현재 위치의 위도, 경도
         lat, lon = gps_values
 
-        #특공관 중앙에서 가장 끝을 기준으로 특공관 범위 내를 원 범위로 계산
+        #중앙에서 가장 끝을 기준으로 주차장 범위 내를 원 범위로 계산
         range = math.sqrt(abs(middle_lat-edge_lat)**2 + abs(middle_lon-edge_lon))
 
-        #특공관 중앙에서 현재 위치까지의 거리 계산
+        #주차장 중앙에서 현재 위치까지의 거리 계산
         length = math.sqrt(abs(middle_lat - lat)**2 + abs(middle_lon - lon)**2)
 
-        #현재 위치가 특공관 범위 내에 있다면 True를 출력
+        #현재 위치가 범위 내에 있다면 True를 출력
         if length <= range : 
             return True
         else :
