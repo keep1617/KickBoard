4eaf37a86966f1b57ef0372a949a9c8e42b5af61
201824524 <150095692+201824524@users.noreply.github.com>
2023-12-06 16:19:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class UI():
 
         column_median_list = column_median.tolist()
 
-        print("각 열의 중간값 (NumPy 배열):", column_median)
+
         print("각 열의 중간값 (Python 리스트):", column_median_list)
 
             
@@ -66,6 +66,7 @@ class UI():
         else:
             self.button.pack_forget()
             self.result_label.config(text=f'반납실패')
+            self.result_label.config(text=f'rgb : {sensing_result} gps:{location_result}')
             self.play_failed()
             self.button1 = Button(self.tk, text='다시 반납', command = lambda:self.show_result())
             self.button1.pack(side = 'left', padx=10, pady=10)
@@ -139,7 +140,6 @@ class Arduino:
 #end here
 
 
-#start here
 class Judge:
     def __init__(self) :
         self.RGB_sensor = []
@@ -149,8 +149,8 @@ class Judge:
     def sensing(self, sensor_values):
         self.RGB_sensor = sensor_values
 
-        lower = np.array([41, 26, 17])  # BGR
-        upper = np.array([57, 37, 28])
+        lower = np.array([30, 30, 30])  # BGR
+        upper = np.array([40, 40, 40])
         rgb123 = np.array(self.RGB_sensor) 
 
 
@@ -161,34 +161,36 @@ class Judge:
         
     
     def location(self, gps_values) :
-        #GPS 값이 웅비관 킥보드 주차장 위치에 해당하면 True를 출력
-        #주차장 중앙의 위도, 경도 값을 중심으로 일정 범위 내에 있는지 확인
+        #GPS 값이 특공관 위치에 해당하면 True를 출력
+        #특공관 중앙의 위도, 경도 값을 중심으로 일정 범위 내에 있는지 확인
 
         self.GPS_val = gps_values
 
-        #웅비관 킥보드 주차장 중앙의 위도, 경도
+        #특공관 중앙의 위도, 경도
         middle_lat = 35.237052
         middle_lon = 129.077633
 
-        #웅비관 킥보드 주차장 가장 끝의 위도, 경도
-        edge_lat = 35.236781
-        edge_lon = 129.077513
+        #특공관 가장 끝의 위도, 경도
+        edge_lat = 35.236867
+        edge_lon = 129.077564
 
         #현재 위치의 위도, 경도
         lat, lon = gps_values
 
-        #중앙에서 가장 끝을 기준으로 주차장 범위 내를 원 범위로 계산
+        #특공관 중앙에서 가장 끝을 기준으로 특공관 범위 내를 원 범위로 계산
         range = math.sqrt(abs(middle_lat-edge_lat)**2 + abs(middle_lon-edge_lon))
 
-        #주차장 중앙에서 현재 위치까지의 거리 계산
+        #특공관 중앙에서 현재 위치까지의 거리 계산
         length = math.sqrt(abs(middle_lat - lat)**2 + abs(middle_lon - lon)**2)
 
-        #현재 위치가 범위 내에 있다면 True를 출력
+        #현재 위치가 특공관 범위 내에 있다면 True를 출력
         if length <= range : 
             return True
         else :
             return False
         
+        
+#start here
 
 bool =True
 ui = UI(bool) 
